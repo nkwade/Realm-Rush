5b50abbd92bac818acd309c940249d3b970ed4f8
beakerwade <beaker.wade@gmail.com>
2022-03-16 19:08:10 -0400

    setup ui

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] int startBal = 150;
-    
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     int currBal;
     public int CurrBal {get { return currBal;} }
 
     private void Awake() {
         currBal = startBal;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount) {
         currBal += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount) {
@@ -25,10 +29,15 @@ public class Bank : MonoBehaviour
             //Lose game
 
         }
+        UpdateDisplay();
     }
 
     private void ReloadScene() {
         Scene curScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(curScene.buildIndex);
+    }
+
+    void UpdateDisplay() {
+        displayBalance.text = "Gold: " + currBal;
     }
 }
